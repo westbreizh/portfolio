4af2633d978c7e24d00e828c6b31562001126916
aurélien <herbreteauaurelien@hotmail.fr>
2022-11-21 15:10:19 +0100

    first coco

---
+++ src/data.js
@@ -0,0 +1,63 @@
+export const projects = [
+
+  {
+    title: "Oh My Food !",
+    subtitle: "site de restauration en ligne",
+    description:
+     "" ,
+    image: "ohmyfood.png",
+    link: "https://westbreizh.github.io/P3-food/",
+  },
+  {
+    title: "La panthère web design",
+    subtitle: "site d'une agence de web design",
+    description:"",
+      
+    image: "./logo.webp",
+    link: "https://westbreizh.github.io/P4_panthere/index.html",
+  },
+  {
+    title: "Kanap",
+    subtitle: "Site e-commerce de canapé",
+    description:
+      "",
+    image: "kanap_p3.png",
+    link: "https://github.com/westbreizh/P5_kanap",
+  },
+  {
+    title: "Kasa",
+    subtitle: "site de location de logements",
+    description:
+      " site réalisé avec la technologie React",
+    image: "./LOGO.png",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML 5",
+  "CSS 3",
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "Mysql",
+  "Material UI",
+];
