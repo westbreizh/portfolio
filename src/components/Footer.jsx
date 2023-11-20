80bc1a3cd8cee73ace7cbd520a146f3e436cf7b5
westbreizh <herbreteauaurelien@hotmail.fr>
2023-11-20 16:27:37 +0100

    modif

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Footer = ({ sharedBasicInfo }) => {
+  let networks = null;
+
+  if (sharedBasicInfo) {
+    networks = sharedBasicInfo.social.map((network) => (
+      <span key={network.name} className="m-4">
+        <a href={network.url} target="_blank" rel="noopener noreferrer">
+          <i className={network.class}></i>
+        </a>
+      </span>
+    ));
+  }
+
+  return (
+    <footer>
+      <div className="col-md-12">
+        <div className="social-links">{networks}</div>
+
+        <div className="copyright py-4 text-center">
+          <div className="container">
+            <small>
+              Copyright &copy;{" "}
+              {sharedBasicInfo ? sharedBasicInfo.name : "???"}
+            </small>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
